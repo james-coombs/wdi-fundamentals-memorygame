08c889d2ca7b961f02f1e550a472702484c5867b
james-coombs <coombsj4@yahoo.com>
2016-08-21 11:44:09 -0400

    added game logic alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,19 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardFour); {
+	alert("Sorry, try again.");
+}
+
+if (cardOne === cardThree); {
+	alert("Sorry, try again.");
+}
+
+if (cardOne === cardTwo); {
+	alert("You found a match!");
+}
+
+if (cardOne === cardTwo); {
+	alert("You found a match!");
+}
